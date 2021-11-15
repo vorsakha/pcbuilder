368076700cc6a412512ec9da36b8ee80951e2826
vorsakha <andreitf.dev@gmail.com>
2021-11-15 14:01:12 -0300

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        tech: ["Share Tech", "sans-serif"],
+        cairo: ["Cairo", "Share Tech", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
